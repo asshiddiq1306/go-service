b6f633115fd68e3194a248b780dd5ed425054f47
Asshiddiq <asshiddiq1306@gmail.com>
2021-07-15 09:15:16 +0700

    add and update product using standard lib

--- data/product.go
+++ data/product.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -19,9 +20,45 @@ type Product struct {
 
 type Products []*Product
 
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
+}
+
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
+func UpdateProduct(id int, p *Product) error {
+	i, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[i] = p
+	return nil
+}
+
+var ErrorNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return i, nil
+		}
+	}
+	return -1, ErrorNotFound
 }
 
 func GetProduct() Products {
